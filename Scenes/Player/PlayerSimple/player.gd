9fb0089faa359f017e1f7eabbdfec8beaab27612
Thys Kok <T.A.Kok@student.tudelft.nl>
2023-10-05 20:25:53 +0200

    Change simplePlayer controls to be a bit more refined
    Also, make sure to reassign the player path variable of the enemy node everytime you try a different player layout

--- Scenes/Player/PlayerSimple/player.gd
+++ Scenes/Player/PlayerSimple/player.gd
@@ -1,43 +1,69 @@
 extends CharacterBody3D
 
+@onready var animations = $AnimationPlayer
 @onready var gunRay = $Head/Camera3d/RayCast3d as RayCast3D
-@onready var Cam = $Head/Camera3d as Camera3D
+@onready var cam = $Head/Camera3d as Camera3D
+@onready var head = $Head
+
 @export var _bullet_scene : PackedScene
+
+var speed
 var mouseSensibility = 1200
 var mouse_relative_x = 0
 var mouse_relative_y = 0
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var push_force = 80
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.0
+const JUMP_VELOCITY = 5.5
+const GROUND_INERTIA = 7.0
+const AIR_INERTIA = 3.5
+const gravity = 11
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	#Captures mouse and stops rgun from hitting yourself
 	gunRay.add_exception(self)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
+	# Handle Jump
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Handle Shooting
 	if Input.is_action_just_pressed("Shoot"):
 		shoot()
+	# Handle Sprinting
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * GROUND_INERTIA)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * GROUND_INERTIA)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * AIR_INERTIA)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * AIR_INERTIA)
+	
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody3D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 
 func _input(event):
 	if event is InputEventMouseMotion:
