e1d9bfb774eb801d61ff9681ab6fc2837a2f12fc
Thys Kok <T.A.Kok@student.tudelft.nl>
2023-10-11 14:57:10 +0200

    Continue work on level

--- levels/Ammo.gd
+++ levels/Ammo.gd
@@ -6,6 +6,7 @@ var max_bullets = 10
 func set_bullet_counts(new_bullet_count):
 	bullets = new_bullet_count
 	max_bullets = new_bullet_count
+	update_bullet_counts()
 
 func on_shoot():
 	bullets -= 1
