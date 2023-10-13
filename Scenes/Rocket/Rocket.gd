43431796dc5e316285006564fe77c2f9a81009c2
T-Dekker <thijs.wim@ziggo.nl>
2023-10-13 22:30:04 +0200

    Readability change

--- Scenes/Rocket/Rocket.gd
+++ Scenes/Rocket/Rocket.gd
@@ -15,11 +15,12 @@ func _on_body_entered(_body):
 	if running: 
 		objectHit.emit(position)
 	
+	# Rocket hit => sound/particles/decal
 	set_inactive()
-	plant_decal()
 	$AudioStreamPlayer3D.pitch_scale = randf_range(1,1.5)
 	$AudioStreamPlayer3D.play()
 	$ExplosionParticles.emitting = true
+	plant_decal()
 	await $AudioStreamPlayer3D.finished
 	queue_free()
 
@@ -27,11 +28,13 @@ func plant_decal():
 	if ($RayCast3D.get_collider() == null):
 		return
 	
+	# Plant a new decal
 	var new_decal = _decal.instantiate()
 	get_tree().get_root().add_child(new_decal) 
 	new_decal.global_position = $RayCast3D.get_collision_point()
 	var normal = $RayCast3D.get_collision_normal()
 	# Figuring this out took way too long
+	# Align the decal to the normal vector
 	if normal != Vector3.UP:
 		new_decal.look_at(new_decal.global_position + normal, Vector3.UP)
 		new_decal.transform = new_decal.transform.rotated_local(Vector3.RIGHT, PI/2)
