f8e3727a87616e56e5217da573764a6719b05f55
Thys Kok <T.A.Kok@student.tudelft.nl>
2023-10-14 18:35:16 +0200

    Add UI for resetting and resetting itself

--- Scenes/Player/LimitedAmmoPlayer.gd
+++ Scenes/Player/LimitedAmmoPlayer.gd
@@ -1,14 +1,25 @@
 extends "res://Scenes/Player/PlayerBlastControl2/playerBlastControl2.gd"
 
+signal no_bullets
+signal player_died
+
 var bullets
-var max_bullets = 20
+var max_bullets = 2
+var reset_time = 0
 
 func _ready():
 	bullets = max_bullets
 	$"../UI/Ammo".set_bullet_counts(max_bullets)
 	$"../UI/Ammo".show()
 	super._ready()
-	
+
+func _process(delta):
+	if Input.is_action_pressed("reset"):
+		reset_time += delta
+	else:
+		reset_time = 0
+	if reset_time >= 2:
+		death()
 
 func shoot():
 	if bullets <= 0:
@@ -16,7 +27,12 @@ func shoot():
 	bullets -= 1
 	$"../UI/Ammo".on_shoot()
 	super.shoot()
+	if bullets == 0:
+		emit_signal("no_bullets")
 
+func death():
+	emit_signal("player_died")
+	super.death()
 
 func _on_bullet_refill_body_entered(body):
 	if body.name == "Player":
