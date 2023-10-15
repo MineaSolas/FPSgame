2f2b60876f15804363165c81ab20df2610a19d7f
Lenka Hake <l.hake@student.tudelft.nl>
2023-10-15 12:41:02 +0200

    Fix resource path for targets

--- Scenes/Target/Target.gd
+++ Scenes/Target/Target.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 signal hit_signal
 
 @onready var target_mesh: MeshInstance3D = $"../MeshInstance3D"
-@onready var mesh_target_hit = load("res://assets/target_hit.obj")
+@onready var mesh_target_hit = load("res://assets/meshes/target_hit.obj")
 
 @export var has_been_hit = false :
 	set(value):
