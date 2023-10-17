97e135053d52412b5851400ac3cb0676a5f2f4bc
Arno van Liere <arnovliere@gmail.com>
2023-10-17 23:42:22 +0200

    Move target hit count logic to player script

--- levels/level5/Level5.gd
+++ levels/level5/Level5.gd
@@ -3,34 +3,14 @@ extends Node3D
 @onready var fade = $Fade
 @onready var player = $Player
 
-var total_targets = 7
-var hit_targets = 0
-@onready var target_counter = player.targetCounter
-
 func _ready():
 	player.progress.selected_lvl = 5
-
-	var targets = get_tree().get_nodes_in_group("Targets")
-	total_targets = targets.size()
-	for target in targets:
-		target.connect("hit_signal", hit_target)
-
-	set_target_counter_text()
-	target_counter.show()
+	player.finish_level_on_all_targets_hit(level_passed)
 
 	fade.show_self()
 	fade.fade_in()
 	
 	player.start_timer()
-
-func hit_target():
-	hit_targets += 1
-	set_target_counter_text()
-	if hit_targets == total_targets:
-		level_passed()
-
-func set_target_counter_text():
-	target_counter.text = "Targets: " + ("%d" % hit_targets) + "/" + str(total_targets)	
 
 func level_passed():
 	player.stop_timer()
