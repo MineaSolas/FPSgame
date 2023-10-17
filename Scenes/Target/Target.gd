a6954b02cacf7055e90d8493b84f271f36470666
Thys Kok <T.A.Kok@student.tudelft.nl>
2023-10-17 18:33:50 +0200

    Add sounds to targets

--- Scenes/Target/Target.gd
+++ Scenes/Target/Target.gd
@@ -4,6 +4,9 @@ signal hit_signal
 
 @onready var target_mesh: MeshInstance3D = $"../MeshInstance3D"
 @onready var mesh_target_hit = load("res://assets/meshes/target_hit.obj")
+@onready var audio_stream_player = $"../AudioStreamPlayer3D"
+
+var target_hit_sound = preload("res://assets/sounds/394795__lanooskiproductions__ding.wav")
 
 @export var has_been_hit = false :
 	set(value):
@@ -14,6 +17,8 @@ signal hit_signal
 func parent_hit():
 	if not has_been_hit:
 		has_been_hit = true
+		audio_stream_player.stream = target_hit_sound
+		audio_stream_player.play()
 
 func target_has_been_hit():
 	return has_been_hit
